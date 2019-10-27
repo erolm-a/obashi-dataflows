10419f65722480f778f9452198de000e58ad8e42
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2019-10-27 22:51:48 +0000

    Basic Unity project with indoor moving minimap.

---
+++ Assets/Scripts/MainARController.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainARController : MonoBehaviour
+{
+    private bool m_IsQuitting;
+    private Vector3 m_PrevPosition;
+
+    //public Camera firstPersonCamera;
+    public Camera mappingCamera;
+    public GameObject player;
+
+    int frameCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_IsQuitting = false;
+        m_PrevPosition = Vector3.zero;
+        frameCounter = 0;
+        Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _UpdateApplicationLifecycle();
+
+        Vector3 curPosition = Frame.Pose.position;
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            m_PrevPosition = curPosition;
+        }
+        else
+        {
+            var deltaPosition = curPosition - m_PrevPosition;
+            player.transform.Translate(deltaPosition.x, 0, deltaPosition.z);
+            
+            m_PrevPosition = curPosition;
+            frameCounter++;
+
+            if (frameCounter % 10 == 0)
+            {
+                Debug.Log($"{curPosition.x} {curPosition.y} {curPosition.z}");
+                Debug.Log($"Player position: {player.transform.position}");
+            }
+        }
+        
+    }
+
+    /// <summary>
+    /// Check and update the application lifecycle.
+    /// </summary>
+    private void _UpdateApplicationLifecycle()
+    {
+        // Exit the app when the 'back' button is pressed.
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // Only allow the screen to sleep when not tracking.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            Screen.sleepTimeout = SleepTimeout.SystemSetting;
+        }
+        else
+        {
+            Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        }
+
+        if (m_IsQuitting)
+        {
+            return;
+        }
+
+        // Quit if ARCore was unable to connect and give Unity some time for the toast to
+        // appear.
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+        else if (Session.Status.IsError())
+        {
+            _ShowAndroidToastMessage(
+                "ARCore encountered a problem connecting.  Please start the app again.");
+            m_IsQuitting = true;
+            Invoke("_DoQuit", 0.5f);
+        }
+    }
+
+    /// <summary>
+    /// Actually quit the application.
+    /// </summary>
+    private void _DoQuit()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void _ShowAndroidToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject unityActivity =
+            unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (unityActivity != null)
+        {
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                AndroidJavaObject toastObject =
+                    toastClass.CallStatic<AndroidJavaObject>(
+                        "makeText", unityActivity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -14,4 +14,24 @@
 
 ## Project description
 
-To be defined
+To be defined (see #11)
+
+# Setup
+
+0. Install the Android SDK. You don't need android studio, but if you wish you can install it as well.
+
+1. Download Unity Hub.
+
+2. Create an account.
+
+3. Install **Unity 2018.4.11f1.**
+
+4. Clone the project and have it open by Unity Hub. You may get lots of import errors now, just ignore them.
+
+5. Install ARCore SDK for Unity (https://developers.google.com/ar/develop/unity/quickstart-android) but do not open HelloAR.scene, just open `Navigation.scene` under `Assets/Scenes`. Instead, skip and setup Unity to detect your SDK.
+
+6. Hopefully you should be able to launch a build now.
+
+7. On your phone, install "Google Play Services for AR".
+
+8. Install the generated apk at step 6 (either via adb or via issuing the 'build and run' command inside Unity).
