d385aa169b7045edac7800905b91e847b6ede965
Enrico "erolm_a" Trombetta <trombetta.enricom@protonmail.com>
2020-02-09 16:00:04 +0000

    Do not depend on Examples anymore, #42

---
+++ Assets/Scripts/Common/Device.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace DataFlows
+{
+
+    public enum OSIStack
+    {
+        DATA_LINK,
+        IP,
+        TRANSPORT,
+        APPLICATION
+    }
+
+    public enum DeviceType
+    {
+        PC,
+        ROUTER,
+        SWITCH,
+        SERVER,
+        PRINTER
+    }
+
+    public abstract class Device : MonoBehaviour
+    {
+        public OSIStack osiType { get; private set; }
+        public string deviceName { get; private set; }
+
+        public void Populate(OSIStack osiType, string deviceName)
+        {
+            this.osiType = osiType;
+            this.deviceName = deviceName;
+        }
+
+        /// <summary>
+        /// Action to perform when the object is selected
+        /// </summary>
+        public abstract void OnUserSelect();
+    }
+}
